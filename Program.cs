c87f5b725ead61f787c618401f7bf6f22548ca8a
Sofia <masha.2153a@mail.ru>
2024-07-27 19:33:23 +0300

    написала программу чат бот

--- Program.cs
+++ Program.cs
@@ -10,8 +10,40 @@ namespace HomeWorkGitSofiya
     {
         static void Main(string[] args)
         {
-            Console.Write("Hi");
-            string msg = Console.ReadLine();
+            string userFeel, userMsg;
+            Console.WriteLine("как дела?");
+            userMsg = Console.ReadLine();
+            userFeel = userMsg;
+            if (userFeel == "хорошо")
+            {
+                Console.WriteLine("что у вас случилось хорошео?");
+            
+            }
+            else if (userFeel == "плохо")
+            {
+                Console.WriteLine("что у вас случилось плохого?");
+                
+               
+            }
+            else
+            {
+                Console.WriteLine("продолжай");
+            }
+            userMsg = Console.ReadLine();
+            if (userFeel == "хорошо")
+            {
+                Console.WriteLine("круто");
+            }    
+           else if (userFeel == "плохо"){
+
+                Console.WriteLine("бывает");
+
+            }
+           else
+            {
+                Console.WriteLine("ну ладно");
+            }
         }
+
     }
 }
