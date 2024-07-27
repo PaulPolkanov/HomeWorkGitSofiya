9ccc5f894c0f51825734982edb98c2918d2d5baa
Pavel Polkanov <63466418+PaulPolkanov@users.noreply.github.com>
2024-07-27 19:48:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,39 +10,13 @@ namespace HomeWorkGitSofiya
     {
         static void Main(string[] args)
         {
-            string userFeel, userMsg;
-            Console.WriteLine("как дела?");
-            userMsg = Console.ReadLine();
-            userFeel = userMsg;
-            if (userFeel == "хорошо")
-            {
-                Console.WriteLine("что у вас случилось хорошео?");
-            
-            }
-            else if (userFeel == "плохо")
-            {
-                Console.WriteLine("что у вас случилось плохого?");
-                
-               
-            }
-            else
-            {
-                Console.WriteLine("продолжай");
-            }
-            userMsg = Console.ReadLine();
-            if (userFeel == "хорошо")
-            {
-                Console.WriteLine("круто");
-            }    
-           else if (userFeel == "плохо"){
-
-                Console.WriteLine("бывает");
-
-            }
-           else
-            {
-                Console.WriteLine("ну ладно");
-            }
+            /*
+                Название ветки: HomeWork27-07
+                Ветка: bugFix-HomeWork27-07
+                Задание: Написать программу 
+            */
+            //code
+            //
         }
 
     }
